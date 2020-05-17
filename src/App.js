402ba9e2e13536175a015d17a06169b7d826fc77
Ronan Binny <binnyronan@gmail.com>
2020-05-18 11:11:14 +1200

    Intial development state for project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="container">
+    </div>
+    </Router>
+  );
+}
+
+export default App;
