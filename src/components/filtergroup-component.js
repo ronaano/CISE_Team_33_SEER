a9a9b3c5041b14830dc851232cec6b742ca73bb1
Ronan Binny <binnyronan@gmail.com>
2020-05-31 00:00:40 +1200

    added fitergroup component, more state data set

---
+++ src/components/filtergroup-component.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+export default class FilterGroup extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state({
+            select1value: " ",
+            select2value: " ",
+            select3value: " "
+        });
+    }
+    
+    render() {
+        (
+        <div>
+            <select>
+            {this.props.select1.map(options => {
+                return <option key={options.name} value={options.name} /> }
+            )}
+            </select>
+            <select>
+            {this.props.select2.map(options2 => {
+                return <option key={options2.name} value={options2.name} /> }
+            )}
+            </select>
+            <select>
+            {this.props.select3.map(options3 => {
+                return <option key={options3.name} value={options3.name} /> }
+            )}
+            </select>
+        </div>
+        )
+    }
+}
+
--- src/components/search-evidence-records-component.js
+++ src/components/search-evidence-records-component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import FilterGroup from './filtergroup-component';
+
+const filterGroups = [{ id: 1 , select1 : "", select2: "", select3: ""}];
 
 export default class SearchEvidenceRecords extends Component {
     constructor(props) {
@@ -11,33 +14,43 @@ export default class SearchEvidenceRecords extends Component {
             semethodology: ["Scrum", "Waterfall", "Spiral", "XP"],
             participants: ["Undergraduate Students", "Postgraduate Students", "Pracitioners"],
             remethod: ["Case Study", "Field Observation", "Experiment", "Interview", "Survey"],
-            benefit: [],
-            context: [],
-            result: "",
             operators: ["contains", "does not contain", "begins with", "ends with", "is equal to"],
-            operators2: ["AND", "OR"]
+            operators2: ["AND", "OR"],
+            results: []
         };
+    }
+    
+    componentDidMount(){
+
     }
 
     onSubmit(event) {
         event.preventDefault();
+        axios.get("http://localhost:5000/evidencerecord/search").
+        then(response => {
+            this.setState({
+            results: response.data});
+        })
+        .catch(err => console.log(err + http_response_code(400)));
     }
 
     addFilter(){
-        
+       // filterGroups.push({this.prevState.id+1, select1 : "", select2: "", select3: ""});
     }
 
     removeFilter(){
-
+        filterGroups.pop();
     }
+
 
     render() {
         return (
             <form onSubmit={this.onSubmit}>
-                <input value={this.state.search}></input>
-                <button onClick={removeFilter}>-</button>
+                <input value={this.state.search}>Description</input>
+                {filterGroups.map((filterGroup) => {
+                <FilterGroup key={filterGroup.id} select1={this.state.filters} select2={this.state.operators} select3={} />})}
                 <button onClick={addFilter}>+</button>
-                <input type="submit" value="Search"/>
+                <input type="submit" value="Search" onChange={this.onSubmit}/>
             </form>
         );
     }
